b9976778a82e1bc0e0f15e6482d6a99e4fa92425
prathusingh <prathusingh21@gmail.com>
2025-01-27 21:19:14 -0500

    updated bitcoin impl

--- bitcoinimpl/main.go
+++ bitcoinimpl/main.go
@@ -7,30 +7,23 @@ import (
 
 // Block structure
 type Block struct {
-		Index int // Block number
-		Timestamp time.Time // Block creation time
-		Transactions []string // List of transactions
-		PreviousHash string // Hash of the previous block
-		Hash string // Current block hash
-		Nonce int // Proof of work nonce
+	Index int // Block number
+	Timestamp time.Time // Block creation time
+	Data string // Data (Eg transaction details)
+	PrevHash string // Hash of the previous block
+	Hash string // Current block hash
+	Nonce int // Proof of work nonce
 }
 
 // Blockchain structure
-var Blockchain []Block
-
-// Create a new block
-func NewBlock(transactions []string, previousHash string) Block {
-		block := Block{
-			Index: len(Blockchain),
-			Timestamp: time.Now(),
-			Transactions: transactions,
-			PreviousHash: previousHash,
-		}
-
-		block.Hash, block.Nonce = "", 1
-
-		return block
+type Blockchain struct {
+	Chain []Block
+	CurrentTransactions []string
 }
+
+
+
+
 
 
 func main() {
